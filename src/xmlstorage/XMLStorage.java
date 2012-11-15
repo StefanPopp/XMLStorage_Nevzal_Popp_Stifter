98c6395beb0d169ecb6eff3b088ce65540445b01
MacMario <MacMario@MM-NOTEBOOK>
2012-11-15 16:42:42 +0100

    Commit this!

--- src/xmlstorage/XMLStorage.java
+++ src/xmlstorage/XMLStorage.java
@@ -15,5 +15,6 @@ public class XMLStorage {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Mario hat was geändert");
     }
 }
