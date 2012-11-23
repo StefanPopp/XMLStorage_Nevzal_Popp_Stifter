bbd3800ee3f2e149eba60afa210c12d1718ce430
stefanpopp <stefanpopp@10.201.68.76>
2012-11-23 17:28:43 +0100

    IStorage hinzugefügt XMLStorage von Interface erstellt

--- src/xmlstorage/XMLStorage.java
+++ src/xmlstorage/XMLStorage.java
@@ -4,16 +4,51 @@
  */
 package xmlstorage;
 
+import java.util.List;
+import javax.rad.model.SortDefinition;
+import javax.rad.model.condition.ICondition;
+import javax.rad.persist.DataSourceException;
+import javax.rad.persist.MetaData;
+
 /**
  *
  * @author stefanpopp
  */
-public class XMLStorage {
+public class XMLStorage implements IStorage{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    @Override
+    public MetaData getMetaData() {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    @Override
+    public int getEstimatedRowCount(ICondition pFilter) throws DataSourceException {
+        throw new DataSourceException("Not supported yet.");
+    }
+
+    @Override
+    public List<Object[]> fetch(ICondition pFilter, SortDefinition pSort, int pFromRow, int pMinimumRowCount) throws DataSourceException {
+        throw new DataSourceException("Not supported yet.");
+    }
+
+    @Override
+    public Object[] refetchRow(Object[] pDataRow) throws DataSourceException {
+        throw new DataSourceException("Not supported yet.");
+    }
+
+    @Override
+    public Object[] insert(Object[] pDataRow) throws DataSourceException {
+        throw new DataSourceException("Not supported yet.");
+    }
+
+    @Override
+    public Object[] update(Object[] pOldDataRow, Object[] pNewDataRow) throws DataSourceException {
+        throw new DataSourceException("Not supported yet.");
+    }
+
+    @Override
+    public void delete(Object[] pDeleteDataRow) throws DataSourceException {
+        throw new DataSourceException("Not supported yet.");
+    }
+   
 }
